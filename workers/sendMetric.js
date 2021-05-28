4785ff7aac7869a9a3d8288eda475e1f367377a6
Trevor Wong <trevor8568@gmail.com>
2021-05-27 20:34:16 -0500

    another attempt at fixing request

--- workers/sendMetric.js
+++ workers/sendMetric.js
@@ -1,18 +1,18 @@
+const { json } = require('express');
 const fetch = require('node-fetch');
 
 module.exports = {
     name: 'sendMetric',
     run(metric, data, app) {
-        fetch(`https://api.statuspage.io/v1/pages/${app.config.statuspage.page_id}/metrics/${metric}/data.json`, {
+        fetch(`https://api.statuspage.io/v1/pages/${app.config.statuspage.page_id}/metrics/${metric}/data`, {
                 method: 'post',
-                body: {
-                    data: {
-                        timestamp: Date.now() / 1000,
-                        value: data
-                    }
-                },
+                body: JSON.stringify({
+                    timestamp: Date.now() / 1000,
+                    value: data
+                }),
                 headers: {
-                    'Authorization': 'OAuth ' + app.config.statuspage.api_key
+                    'Content-Type': 'application/json',
+                    'Authorization': app.config.statuspage.api_key
                 },
             })
             .then(app.log.info(`Submitted metric to statuspage`))
